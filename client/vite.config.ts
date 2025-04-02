b2bca2cfcf83711177d29a64d55dbe2237bdcc0d
Max-Yfantopoulos <maxyfantopoulos@berkeley.edu>
2025-04-02 15:05:10 +0200

    fixed vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const isProduction = mode === "production";
-
+export default defineConfig(() => {
   return {
     plugins: [react()],
     server: {
       proxy: {
         "/api": {
-          target: "https://backgammon-website.onrender.com",
+          target: "https://backgammon-website.onrender.com", // Change to "http://localhost:5001" for development and "https://backgammon-website.onrender.com" for production
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ""),
         },
