0017739bb5f51101fbb4d3affeb6f1de2d77ae75
Max-Yfantopoulos <maxyfantopoulos@berkeley.edu>
2025-04-03 23:57:35 +0200

    changed mcts amount

--- flask-server/server.py
+++ flask-server/server.py
@@ -344,4 +344,4 @@ def fetch_color():
 
 if __name__ == "__main__":
     init_db()
-    app.run(host="0.0.0.0", port=5001, debug=True) #Add debug=True for development
+    app.run(host="0.0.0.0", port=5001) #Add debug=True for development
